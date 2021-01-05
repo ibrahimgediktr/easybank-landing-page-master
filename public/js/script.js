a7dc30d6549c8b2d282a30882fe64f80a2b1f9d5
İbrahim Gedik <67916168+ibrahimgediktr@users.noreply.github.com>
2021-01-05 21:25:47 +0300

    fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,6 @@
 const navList = document.querySelector('.nav-list');
 const toggleMenu = document.querySelector('.toggle-menu');
-const openIcon = document.querySelector('.open-icon')
-const closeIcon = document.querySelector('.close-icon')
 
-var imgSrc = document.querySelector('.toggle-icon');
-
-
-
-
-console.log(navList);
-console.log(toggleMenu);
 
 toggleMenu.addEventListener('click', () => {
     navList.classList.toggle('nav-active');
